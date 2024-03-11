f89cae5d7a9db38dba7d4722e3a94230bde88413
Emanuel Vanegas <emanuelvan00@gmail.com>
2024-03-11 02:10:10 -0500

    Cambie to xd

--- calculadora.js
+++ calculadora.js
@@ -1,15 +1,46 @@
 function calculate() {
-    var Tm = parseFloat(document.getElementById('Tm').value)*9.81;//Se convierte a newtons
-    var r = parseFloat(document.getElementById('r').value);
-    var miu = parseFloat(document.getElementById('miu').value);
-    var m = parseFloat(document.getElementById('m').value);
-    var resultRadio2;
-    var resultAceleracion;
-    var g
-    g = 9.81
-    resultRadio2 = Tm / ((miu*m*g)*r)
-    resultAceleracion = Math.pow(resultRadio2,2)*miu/2
-    document.getElementById('resultadorRadio').innerText = "Resultado Radio: " + resultRadio2.toFixed(2);
-    document.getElementById('resultadorAceleracion').innerText = "Resultado Aceleracion: " + resultAceleracion.toFixed(2);
-    
+  var Tm = parseFloat(document.getElementById("Tm").value); //Se convierte a newtons
+  var r = parseFloat(document.getElementById("r").value);
+  var miu = parseFloat(document.getElementById("miu").value);
+  var m = parseFloat(document.getElementById("m").value);
+  var resultRadio2;
+  var resultAceleracion;
+  var g;
+  const I = 0.5 * m * Math.pow(r, 2); //Momento de inercia de la rueda
+  g = 9.81;
+  if (
+    isNaN(Tm) ||
+    isNaN(r) ||
+    isNaN(miu) ||
+    isNaN(m) ||
+    Tm < 0 ||
+    r <= 0 ||
+    miu < 0 ||
+    m <= 0
+  ) {
+    alert(
+      "Por favor, ingrese valores numéricos positivos para torque del motor, radio, coeficiente de fricción y masa del carro."
+    );
+    return;
+  }
+  resultAceleracion = (Tm * r) / (I * miu * g);
+  console.log("Inercia: ", I);
+  resultRadio2 = Math.sqrt((Tm * I) / (resultAceleracion * miu * g));
+  document.getElementById("resultadorRadio").innerText =
+    "Resultado Radio: " + resultRadio2.toFixed(2);
+  document.getElementById("resultadorAceleracion").innerText =
+    "Resultado Aceleracion: " + resultAceleracion.toFixed(2);
 }
+
+/* const I = 0.5 * m * Math.pow(r,2); //Momento de inercia de la rueda
+  
+  // Handle invalid input (non-numeric values, negative values, etc.)
+  if (isNaN(tm) || isNaN(r) || isNaN(miu) || isNaN(m) || tm < 0 || r <= 0 || miu < 0 || m <= 0) {
+    alert("Por favor, ingrese valores numéricos positivos para torque del motor, radio, coeficiente de fricción y masa del carro.");
+    return;
+  }
+
+  const a = (tm * r) / (I * miu * g);
+  console.log("Resultado aceleracion: ", a);
+  const r2 = Math.sqrt((tm * I) / (a * miu * g)) 
+   console.log("Resultado: ", r2); */
--- main_three.js
+++ main_three.js
@@ -1,51 +1,55 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const loader = new GLTFLoader();
-
+let camera;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer({alpha: true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-document.getElementById("Carrito3D").appendChild(renderer.domElement);
+camera = new THREE.PerspectiveCamera(
+  60,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  10000
+);
+camera.position.set(-1.3, 1, -2);
+const renderer = new THREE.WebGLRenderer({ alpha: false });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+document.getElementById("Carrito_3D").appendChild(renderer.domElement);
 
 let carro;
 let controls;
 
-
-
 loader.load(
-    "Modelo/Monster_rc.glb",
-    function (gltf) {
-        carro = gltf.scene;
-        scene.add(carro)
-    },
-    function(xhr){
-        console.log((xhr.loaded/xhr.total *100)+'% loaded');
-    },
-    function (error) {
-        console.error(error);
-    }
+  "Modelo/Monster_rc.glb",
+  function (gltf) {
+    carro = gltf.scene;
+    scene.add(carro);
+    carro.position.set(0, 0, -1);
+    /*     carro.rotation.y = 3;
+    carro.rotation.x = Math.PI / 4; */
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  function (error) {
+    console.error(error);
+  }
 );
-
-camera.position.z = carro = "carrito"?25:500;
-
-const topLight = new THREE.DirectionalLight(0xffffff, 1)
-topLight.position.set(500,500,500)
+controls = new OrbitControls(camera, renderer.domElement);
+controls.screenSpacePanning = false;
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500, 500, 500);
 topLight.castShadow = true;
 scene.add(topLight);
 
-const ambietnLight = new THREE.AmbientLight(0x33333, carro = "carrito" ? 5:1);
+const ambietnLight = new THREE.AmbientLight(0xffffff);
 scene.add(ambietnLight);
 
-
 function animate() {
-	requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 
-
-	renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 
 animate();
